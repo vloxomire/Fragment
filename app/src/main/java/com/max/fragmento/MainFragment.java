4f4ddaac530c6e2b838b724fea5de3cbb634c41f
max <vloxomire@gmail.com>
2018-09-20 19:35:36 -0300

    Inicio

---
+++ app/src/main/java/com/max/fragmento/MainFragment.java
@@ -0,0 +1,13 @@
+package com.max.fragmento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_fragment);
+    }
+}
