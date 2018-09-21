af7a6ffaffacb0185d93f2f7c10b7fe163871baf
max <40814622+vloxomire@users.noreply.github.com>
2018-09-21 10:37:52 -0300

    10:37am

--- app/src/main/java/com/max/fragmento/Activitis/MainActivity.java
+++ app/src/main/java/com/max/fragmento/Activitis/MainActivity.java
@@ -1,5 +1,7 @@
 package com.max.fragmento.Activitis;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        //Bloque tabLayout
+        TabLayout tabLayout=findViewById(R.id.tabLayoutId);
+        //agrego los tab
+        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+        //seteo gravedad al texto de los tabs
+        tabLayout.setTabGravity(tabLayout.GRAVITY_FILL);
 
+        ViewPager viewPager=findViewById(R.id.viewPagerId);
     }
 }
