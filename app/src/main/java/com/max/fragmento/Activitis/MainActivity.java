91d1c7cb79a90154c96bd03fc97f60d5dc761855
Max <vloxomire@gmail.com>
2018-09-22 01:40:39 -0300

    01:40am

--- app/src/main/java/com/max/fragmento/Activitis/MainActivity.java
+++ app/src/main/java/com/max/fragmento/Activitis/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.max.fragmento.Adapters.PagerAdapter;
 import com.max.fragmento.R;
 
@@ -16,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         //Bloque tabLayout
         TabLayout tabLayout=findViewById(R.id.tabLayoutId);
         //agrego los tab
-        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
-        //seteo gravedad al texto de los tabs
+        tabLayout.addTab(tabLayout.newTab().setText("Chevy"));   //añadimos los tabs
+        tabLayout.addTab(tabLayout.newTab().setText("Falcon"));
+        tabLayout.addTab(tabLayout.newTab().setText("Rastrojero"));
+        //seteo gravedad de los tabs q afectan al texto
         tabLayout.setTabGravity(tabLayout.GRAVITY_FILL);
 
         final ViewPager viewPager=findViewById(R.id.viewPagerId);//vinculado,hay q  crear una clase Adaptador
@@ -34,18 +36,19 @@ public class MainActivity extends AppCompatActivity {
             //define lo q hay q hacer cuando cambiemos de un tab a otro
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this,"Selected->"+ tab.getText(),Toast.LENGTH_SHORT).show();
                 int position=tab.getPosition(); //nos dice en q posicion estamos, nos da un tab de parametro
                 viewPager.setCurrentItem(position); //el viewPager maneja las paginas
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                Toast.makeText(MainActivity.this,"Unselected->"+ tab.getText(),Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
+                Toast.makeText(MainActivity.this,"Reselected->"+ tab.getText(),Toast.LENGTH_SHORT).show();
             }
         });
     }
--- app/src/main/java/com/max/fragmento/Fragments/ChevyFragment.java
+++ app/src/main/java/com/max/fragmento/Fragments/ChevyFragment.java
@@ -5,23 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
 import com.max.fragmento.R;
 
 public class ChevyFragment extends Fragment {
-    private TextView textView1,textView2,textView3;
-    private ImageView imageView;
-    private View view;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-         view=inflater.inflate(R.layout.main_activity, container, false);
-        textView1=getActivity().findViewById(R.id.modeloId);
-        textView2=getActivity().findViewById(R.id.marcaId);
-        textView3=getActivity().findViewById(R.id.anioId);
-        imageView=getActivity().findViewById(R.id.imageViewId);
+        View view=inflater.inflate(R.layout.fragment_chevy, container, false);
+
         return view;
     }
 }
